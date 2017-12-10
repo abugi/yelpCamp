83ee920a9ceef753520b75a955795f5fb9ce5d81
abugi <abugiboi@gmail.com>
2017-12-10 23:00:26 +0100

    initial commit

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ var campgroundRoutes = require("./routes/campground");
 
 
 //seedDB(); //calling the seed.js file
-//mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://abugi:jankara@ds147864.mlab.com:47864/yelpcamp");
+mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://abugi:jankara@ds147864.mlab.com:47864/yelpcamp");
 
 
 //PASSPORT CONFIGURATION
